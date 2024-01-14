a2bd06bb336d78e4897e1b10843cc7afeba1c2d0
BartlomiejPawlowski <barpaw96@gmail.com>
2024-01-14 17:25:19 +0100

    Add project files.

---
+++ Znaki_Specjalne/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Znaki_Specjalne
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {   //ćwiczenie 6.1 str 49
+            double a = 14.5;
+            double b = 24.45;
+            Console.Write("{0}\n"+"{1}\n", a, b);
+            
+            //Ćwiczenie 6.2 str 49
+            Console.WriteLine(" /---\\\t|----\\\t/----\\");
+            Console.WriteLine("|     |\t|    |\t|");
+            Console.WriteLine("|     |\t|    /\t|");
+            Console.WriteLine("|-----|\t|---- \t|");
+            Console.WriteLine("|     |\t|    \\\t|");
+            Console.WriteLine("|     |\t|    |\t|");
+            Console.WriteLine("|     |\t|----/\t\\----/");
+
+            Console.ReadKey();  
+        }
+    }
+}
