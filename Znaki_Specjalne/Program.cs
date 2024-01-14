152a228d71558a4bccc21543c1451d45234bcc98
BartlomiejPawlowski <barpaw96@gmail.com>
2024-01-14 17:35:30 +0100

    Add ćwiczenie 6.3

--- Znaki_Specjalne/Program.cs
+++ Znaki_Specjalne/Program.cs
@@ -24,6 +24,9 @@ namespace Znaki_Specjalne
             Console.WriteLine("|     |\t|    |\t|");
             Console.WriteLine("|     |\t|----/\t\\----/");
 
+            //Ćwiczenie 6.3 str 49
+            Console.WriteLine("111\t+\t222\t=\t333\n" + " + \t\t + \t\t +\n" + "333\t+\t444\t=\t777\n" + " = \t\t = \t\t =\n" + "444\t+\t666\t=\t1110\n");
+
             Console.ReadKey();  
         }
     }
